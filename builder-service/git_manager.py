623004144fc8b20c38b13898a825f7070f20e401
İdil Doğan <idildoga4@hotmail.com>
2026-02-27 18:24:35 +0300

    CI/CD builder servisi ve Docker entegrasyonu tamamlandı

---
+++ builder-service/git_manager.py
@@ -0,0 +1,32 @@
+import subprocess
+import os
+import shutil
+import stat
+
+def on_rm_error(func, path, exc_info):
+    os.chmod(path, stat.S_IWRITE)
+    func(path)
+
+def clone_repo(repo_url, project_name):
+    # İndirilen projeleri tutacağımız ana depo klasörü
+    base_dir = "./workspace"
+    
+    if not os.path.exists(base_dir):
+        os.makedirs(base_dir)
+    
+    project_path = os.path.join(base_dir, project_name)
+    
+    if os.path.exists(project_path):
+        print(f"[*] Old '{project_name}' being deleted (Windows Force)...")
+        shutil.rmtree(project_path, onexc=on_rm_error)
+        
+    print(f"[*]  codes are pulling from '{repo_url}'...")
+    
+    try:
+        subprocess.run(["git", "clone", repo_url, project_path], check=True)
+        print(f"[+] Successful '{project_path}'")
+        return project_path
+    
+    except subprocess.CalledProcessError as e:
+        print(f"[-] An error occurred during Git Clone.: {e}")
+        return None
--- builder-service/main.py
+++ builder-service/main.py
@@ -1,17 +1,38 @@
-# builder-service/main.py
-from flask import Flask
-import time
+from fastapi import FastAPI, BackgroundTasks
+from pydantic import BaseModel
+import git_manager
+import docker_manager
+import requests
 
-app = Flask(__name__)
+app = FastAPI(title="PaaS Builder Service", description="CI/CD Automation Engine")
 
-@app.route('/baslat')
-def baslat():
-    # Burada normalde Docker build işlemi yapılacak.
-    # Şimdilik taklit yapıyoruz.
-    print("LOG: Kod indiriliyor...", flush=True)
-    time.sleep(1)
-    print("LOG: Docker imaji olusturuluyor...", flush=True)
-    return "Basariyla Build Edildi! (Temsili)"
+class DeployRequest(BaseModel):
+    repo_url: str
+    project_name: str
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
+def run_pipeline(repo_url: str, project_name: str):
+    print(f"\n[>>>]For  {project_name} pipeline started [>>>]")
+    
+    project_path = git_manager.clone_repo(repo_url, project_name)
+    if not project_path:
+        print(f"[-] Pipeline stopped: Git Clone is unsucessful.")
+        return
+
+    success = docker_manager.build_and_deploy(project_path, project_name)
+    
+    if success:
+        print(f"[+] Pipeline successful!")
+        # İleride buraya 1. kişinin API'sine başarılı olduğuna dair istek (Webhook) atma kodu eklenecek
+    else:
+        print(f"[-] Pipeline stopped: Docker Build/Run unsucessful.")
+
+@app.post("/deploy")
+async def trigger_deploy(request: DeployRequest, background_tasks: BackgroundTasks):
+    # DİKKAT: İşlemi arka plana atıyoruz. Çünkü git clone ve docker build 
+    # dakikalar sürebilir. Kullanıcıyı web sitesinde bekletemeyiz.
+    background_tasks.add_task(run_pipeline, request.repo_url, request.project_name)
+   
+    return {
+        "status": "success",
+        "message": f"'{request.project_name}' The project has been added to the queue. The build process has started in the background."
+    }
